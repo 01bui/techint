82d6a1dd29a4f40fc7c47cf424f2d0122f1b42bb
Vy Bui <01bui@cardinalmail.cua.edu>
2016-08-31 12:00:22 -0400

    sol

--- quiz.py
+++ quiz.py
@@ -18,6 +18,25 @@ Use the test cases below to guide you!"""
 class Classy(object):
     def __init__(self):
         self.items = []
+        self.classiness = 0
+
+    def getClassiness(self):
+        try:
+            for i in self.items:
+                if i == "tophat":
+                    self.classiness += 2
+                elif i == "bowtie":
+                    self.classiness += 4
+                elif i == "monocle":
+                    self.classiness += 5
+                self.items.pop(0)
+            self.items.pop(0)
+        except IndexError as e:
+            pass
+        return self.classiness
+
+    def addItem(self, item):
+        self.items.append(item)
 
 # Test cases
 me = Classy()
