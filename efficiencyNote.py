eca3cc0505fa7e759f616f1b3b9723cd14992500
Vy Bui <01bui@cardinalmail.cua.edu>
2016-08-31 17:36:05 -0400

    sol

--- efficiencyNote.py
+++ efficiencyNote.py
@@ -25,3 +25,21 @@ def example4(manatees):
             else:
                 oldest_manatee = manatee1['name']
     print oldest_manatee
+
+"""
+Here are the answers and explanations:
+
+Example 1
+We iterate over every manatee in the manatees list with the for loop. Since we're given that manatees has n elements, our code will take approximately O(n) time to run.
+
+Example 2
+We look at two specific properties of a specific manatee. We aren't iterating over anything - just doing constant-time lookups on lists and dictionaries. Thus the code will complete in constant, or O(1), time.
+
+Example 3
+There are two for loops, and nested for loops are a good sign that you need to multiply two runtimes. Here, for every manatee, we check every property. If we had 4 manatees, each with 5 properties, then we would need 5+5+5+5 steps. This logic simplifies to the number of manatees times the number of properties, or O(nm).
+
+Example 4
+Again we have nested for loops. This time we're iterating over the manatees list twice - every time we see a manatee, we compare it to every other manatee's age. We end up with O(nn), or O(n^2) (which is read as "n squared").
+
+Throughout the course, you can reference the Big-O Cheat Sheet to keep track of time complexities for many of the algorithms and data structures we study.
+"""
