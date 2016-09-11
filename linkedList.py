79675e01a123cbd320202229d711bee012c26ebd
Vy Bui <01bui@cardinalmail.cua.edu>
2016-09-10 22:37:14 -0400

    fix insert

--- linkedList.py
+++ linkedList.py
@@ -52,19 +52,33 @@ class LinkedList(object):
             #print count
             #print current.next.value
             current = current.next
-            if count == position-2:
-                #print current.value
-                #print current.next.value
+            if count == position-1:
+                value = current.value
                 current.next = new_element
-                #print current.next.value
+                current.next.next = Element(value)
             count += 1
 
     def delete(self, value):
         """Delete the first node with a given value."""
         current = self.head
-        while current.next:
-            if current.value == value:
-                current.next = None
+        print current.value
+        if not self.head:
+            while current.next:
+                current = current.next
+                print current.value
+                if current.value == value:
+                    current.next = current.next.value
+        else:
+            print "head"
+            self.head = current.next
+            current = self.head
+            print self.head.value
+            print current.value
+            while current.next:
+                current = current.next
+                current.next = current
+                print current.next.value
+
 
 # Test cases
 # Set up some Elements
@@ -80,22 +94,24 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-print ll.head.next.next.value
+#print ll.head.next.next.value
 # Should also print 3
-print ll.get_position(3).value # position in LinkedList
+#print ll.get_position(3).value # position in LinkedList
 #print ll.get_position(4).value # position not in LinkedList
-print "Done get_position"
+#print "Done get_position"
 
 # Test insert
 ll.insert(e4,3)
 # Should print 4 now
+print "Should print 4"
 print ll.get_position(3).value
+print ll.get_position(4).value
 
 # Test delete
-ll.delete(1)
+#ll.delete(1)
 # Should print 2 now
-print ll.get_position(1).value
+#print ll.get_position(1).value
 # Should print 4 now
-print ll.get_position(2).value
+#print ll.get_position(2).value
 # Should print 3 now
-print ll.get_position(3).value
+#print ll.get_position(3).value
