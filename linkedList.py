b945db473034be22001ceef859ae17d562ec9e80
Vy Bui <01bui@cardinalmail.cua.edu>
2016-09-11 21:43:49 -0400

    init del

--- linkedList.py
+++ linkedList.py
@@ -94,11 +94,11 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-#print ll.head.next.next.value
+print ll.head.next.next.value
 # Should also print 3
-#print ll.get_position(3).value # position in LinkedList
+print ll.get_position(3).value # position in LinkedList
 #print ll.get_position(4).value # position not in LinkedList
-#print "Done get_position"
+print "Done get_position"
 
 # Test insert
 ll.insert(e4,3)
@@ -108,10 +108,10 @@ print ll.get_position(3).value
 print ll.get_position(4).value
 
 # Test delete
-#ll.delete(1)
+ll.delete(1)
 # Should print 2 now
-#print ll.get_position(1).value
+print ll.get_position(1).value
 # Should print 4 now
-#print ll.get_position(2).value
+print ll.get_position(2).value
 # Should print 3 now
-#print ll.get_position(3).value
+print ll.get_position(3).value
