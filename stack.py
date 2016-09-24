c16f473e075b66459ac9b997888c22b7eda0e756
Vy Bui <01bui@cardinalmail.cua.edu>
2016-09-23 21:42:15 -0400

    sol

--- stack.py
+++ stack.py
@@ -26,6 +26,7 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
+        new_element.next = self.head
         self.head = new_element
 
     def delete_first(self):
@@ -42,11 +43,11 @@ class Stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        self.ll.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        return self.ll.delete_first()
 
 # Test cases
 # Set up some Elements
