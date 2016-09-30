8886cb44ce3ecd43b808bfa17045545f65867d5e
Vy Bui <01bui@cardinalmail.cua.edu>
2016-09-29 22:18:36 -0400

    init

---
+++ recursion.py
@@ -0,0 +1,12 @@
+"""Implement a function recursivly to get the desired
+Fibonacci sequence value.
+Your code should have the same input/output as the
+iterative code in the instructions."""
+
+def get_fib(position):
+    return -1
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
