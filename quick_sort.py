f678a49e496ac26ad3cdc6e939e9ea9f30c1f699
Vy Bui <01bui@cardinalmail.cua.edu>
2016-10-05 23:29:02 -0400

    init

---
+++ quick_sort.py
@@ -0,0 +1,8 @@
+"""Implement quick sort in Python.
+Input a list.
+Output a sorted list."""
+def quicksort(array):
+    return []
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print quicksort(test)
